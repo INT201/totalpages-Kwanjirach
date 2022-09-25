d3828aae6041e85a093a1b3187aa565f3cad6d95
kwanjirach <kwanjirachandaeng67@gmail.com>
2022-09-25 23:39:22 +0700

    Hw2

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,24 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(rowsPerPage == null || undefined){
+    return 1 
+    // console.log(1)
+    
+}
+else if(arrayItems == null || undefined){
+    return undefined
+    // console.log(undefined)
+    
+}
+else {
+    page = arrayItems.length/rowsPerPage
+    if(page != Infinity){
+        return Math.ceil(page)
+    }else{
+    return 1}
+    // console.log(Math.ceil(page))
+    
+}
 }
 module.exports = totalPages
